a658c0262373e01eacf2c726c558186d18fc05d5
Chinmay Shet <chishxd@tutamail.com>
2026-07-28 03:41:58 +0530

    hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::event;
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| {
+        loop {
+            terminal.draw(|frame| {
+                frame.render_widget("Hello World", frame.area());
+            })?;
+
+            if event::read()?.is_key_press() {
+                break Ok(());
+            }
+        }
+    })
 }
