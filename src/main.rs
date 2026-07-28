b8a750bd5763a67da4db7c454fcf207bc9f987ec
Chinmay Shet <chishxd@tutamail.com>
2026-07-28 18:33:06 +0530

    feat: Use layout to get base TUI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 use std::io;
 
 use crossterm::event;
+use ratatui::{
+    layout::{Constraint, Layout},
+    widgets::{Block, Borders},
+};
 
 fn main() -> io::Result<()> {
     ratatui::run(|terminal| {
         loop {
             terminal.draw(|frame| {
-                frame.render_widget("Hello World", frame.area());
+                let cols = Layout::default()
+                    .direction(ratatui::layout::Direction::Horizontal)
+                    .constraints([
+                        Constraint::Percentage(30),
+                        Constraint::Percentage(50),
+                        Constraint::Percentage(20),
+                    ])
+                    .split(frame.area());
+
+                let left_pane = Block::default().borders(Borders::ALL).title("Left Pane");
+
+                let center_pane = Block::default().borders(Borders::ALL).title("Center Pane");
+                let right_pane = Block::default().borders(Borders::ALL).title("Right Pane");
+
+                frame.render_widget(left_pane, cols[0]);
+                frame.render_widget(center_pane, cols[1]);
+                frame.render_widget(right_pane, cols[2]);
             })?;
 
             if event::read()?.is_key_press() {
